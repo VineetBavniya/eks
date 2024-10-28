a4f2a5560cfb6a8f5e05da39a4cd44a798d19dee
VineetBavniya <138431446+VineetBavniya@users.noreply.github.com>
2024-10-28 13:24:29 +0530

    Update iamrole.tf

--- EKS/iamrole.tf
+++ EKS/iamrole.tf
@@ -47,13 +47,19 @@ resource "aws_iam_role" "eks_worker_node_role" {
   })
 }
 
-
 resource "aws_iam_role_policy_attachment" "eks_worker_amaworker" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.eks_worker_node_role.id
 }
 
+
 resource "aws_iam_role_policy_attachment" "eks_worker_ec2_container" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_worker_node_role.id
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks_worker_cni" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.eks_worker_node_role.id
 }
